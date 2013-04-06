03757dac2332c1d0a2e074cd0dbef4d2146e0e11
ifonefox <ifonefox@gmail.com>
2013-04-05 20:31:09 -0400

    Added more heath methods to Unit

--- src/com/bbr/entity/Unit.java
+++ src/com/bbr/entity/Unit.java
@@ -4,19 +4,34 @@ import com.bbr.core.Zone;
 
 public class Unit extends Entity {
 	public static final int BASE_COLLISION_DAMAGE = 25;
-	protected int health = 0;
+	protected int health = 0; 
 	protected int collisionDamage = BASE_COLLISION_DAMAGE;
-	public Unit(Zone zone, float x, float y) {
+	public Unit(Zone zone, float x, float y, int defaultHealth) {
 		super(zone, x, y);
+		health = defaultHealth;
 	}
-
 	public void hitBy(Entity attacker, int damage) {
-		health -= damage;
+		if(damage > 0)
+			health -= damage;
 	}
 
-	public int getHealth() { return health; }
-	public int getCollisionDamage() { return collisionDamage; }
-
-	public void setHealth(int newHealth) { health = newHealth; }
-	public void setCollisionDamage(int newDamage) { collisionDamage = newDamage; }
+	public int getHealth() {
+		return health;
+	}
+	public int getCollisionDamage() {
+		return collisionDamage;
+	}
+	public void heal(int damageHealed){
+		if(damageHealed > 0)
+			health += damageHealed;
+	}
+	public void setHealth(int newHealth) {
+		health = newHealth;
+	}
+	public boolean isDead(){
+		return health <= 0;
+	}
+	public void setCollisionDamage(int newDamage) {
+		collisionDamage = newDamage;
+	}
 }
