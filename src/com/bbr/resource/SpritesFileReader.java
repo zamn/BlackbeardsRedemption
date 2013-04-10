d1e9dda82cda8c042a21f4b8c775ac1d1757906b
lsftw <lsftw13@gmail.com>
2013-04-10 11:25:57 -0400

    Removed old sprite loading code.

--- src/com/bbr/resource/SpritesFileReader.java
+++ src/com/bbr/resource/SpritesFileReader.java
@@ -14,7 +14,7 @@ public class SpritesFileReader extends SequentialFileReader {
 	public static final Pattern REGEX_DEFAULT_FRAME = Pattern.compile("([^\\.]+\\.[a-zA-Z]+)"); // filename
 	public static final Pattern REGEX_SPECIFIC_FRAME = Pattern.compile("(\\w+)\\s+([^\\.]+\\.[a-zA-Z]+)"); // category filename
 	public static final Pattern REGEX_SPECIFIC_DELAY = Pattern.compile("(\\w+)\\s+(\\d*\\.?\\d*)"); // category delay
-	//private String curCategory = null;
+
 	private Sprite curSprite = null;
 	private String curName = null; // sprite name
 
@@ -36,7 +36,6 @@ public class SpritesFileReader extends SequentialFileReader {
 
 		if (specificFrameMatcher.matches()) {
 			try {
-				System.out.println(curName+":"+specificFrameMatcher.group(1)+"="+specificFrameMatcher.group(2));
 				curSprite.addFrame(specificFrameMatcher.group(1), Art.loadImage(specificFrameMatcher.group(2)));
 			} catch (SlickException e) {
 				e.printStackTrace();
@@ -46,7 +45,6 @@ public class SpritesFileReader extends SequentialFileReader {
 			curSprite.setDelay(specificDelayMatcher.group(1), (int)(Settings.valueInt("fps")*delay));
 		} else if (defaultFrameMatcher.matches()) {
 			try {
-				System.out.println(curName+":"+defaultFrameMatcher.group(1));
 				curSprite.addFrame(Art.loadImage(defaultFrameMatcher.group(1)));
 			} catch (SlickException e) {
 				e.printStackTrace();
@@ -58,51 +56,6 @@ public class SpritesFileReader extends SequentialFileReader {
 			}
 			curName = spriteNameMatcher.group(1);
 		}
-		
-		/*if (curLine.indexOf('.') > 0) { // has delim and text before the delim, is file name
-			int delimPos = curLine.indexOf(' '); // delim between image "name" and image path
-			if (delimPos == -1) {
-				if (curCategory == null) {
-					Utility.printWarning("Loaded a sprite image without a category on line " + lineNumber + " of " + file);
-				} else {
-					try {
-						curSprite.addFrame(Art.loadImage(curLine));
-					} catch (SlickException e) {
-						e.printStackTrace();
-					}
-				}
-			} else {
-				if (curCategory == null) {
-					Utility.printError("Syntax error: Sprite image \"" + curLine + "\" not named in " + Art.SPRITE_LIST + " on line " + lineNumber + " of " + file);
-				} else {
-					float delay = Utility.getFloat(curLine.substring(delimPos+1), -1);
-					if (delay <= 0) {
-						delay = Utility.getInt(curLine.substring(delimPos+1), -1);
-					}
-					if (delay <= 0) {
-						try {
-							curSprite.addFrame(curLine.substring(0,delimPos), Art.loadImage(curLine.substring(delimPos+1)));
-						} catch (SlickException e) {
-							e.printStackTrace();
-						}
-					} else {
-						curSprite.setDelay(curLine.substring(0,delimPos), (long)(Settings.valueInt("fps")*delay));
-					}
-				}
-			}
-		} else {
-			if (curCategory != null) {
-				Art.sprites.put(curCategory, curSprite);
-			}
-			curCategory = curLine;
-			curSprite = Art.getSprite(curCategory);
-			if (curSprite == null) {
-				curSprite = new Sprite();
-			}
-		}
-		if (curCategory != null) {
-			Art.sprites.put(curCategory, curSprite);
-		}*/
 	}
 	protected void endOfFile() {
 		if (curName != null) {
