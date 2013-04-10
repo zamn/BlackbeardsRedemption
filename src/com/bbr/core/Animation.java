3259793ee6ec63c4846fad9eea45448670cc6fec
lsftw <lsftw13@gmail.com>
2013-04-10 10:56:18 -0400

    Removed deprecated time-based animation code.

--- src/com/bbr/core/Animation.java
+++ src/com/bbr/core/Animation.java
@@ -5,13 +5,10 @@ import java.util.ArrayList;
 import org.newdawn.slick.Image;
 
 import com.bbr.resource.Settings;
+import com.bbr.resource.Utility;
 
 public class Animation {
-	// Time-based animation handling - to be deprecated
-	public static final int ANIMATION_FRAME_DELAY = Settings.valueInt("fps") * 10; // time between frames in ms
-	protected long lastTime = 0; // in ms
-	protected long delay = ANIMATION_FRAME_DELAY;
-	// New tick-based/frame-based animation handling
+	// Tick-based/frame-based animation handling
 	protected static TickHandler tickHandler;
 	protected long startTick = 0;
 	protected long tickDelay = Settings.valueInt("fps");
@@ -24,7 +21,6 @@ public class Animation {
 	}
 
 	public void setDelay(long delay) {
-//		this.delay = delay;
 		this.tickDelay = delay;
 	}
 	public void addFrame(Image frame) {
@@ -32,19 +28,14 @@ public class Animation {
 	}
 	public Image getCurrentFrame() {
 		if (frames.size() == 0) return null;
-		if (tickHandler == null) { // to be deprecated
-			if (System.currentTimeMillis() - lastTime >= delay || lastTime == 0) {
-				curFrameIndex++;
-				curFrameIndex %= frames.size(); // loop back to start
-				lastTime = System.currentTimeMillis();
-			}
-			return frames.get(curFrameIndex);
-		} else {
+		if (tickHandler != null) {
 			long frameIndex = (tickHandler.getTickCount() - startTick) / tickDelay;
 			frameIndex = frameIndex % frames.size();
 			// casting to int results in no change since frames.size() is an int
 			return frames.get((int)frameIndex);
 		}
+		Utility.printError("No tick handler attached to animation. Sprites will only show first frame.");
+		return frames.get(0);
 	}
 	public void restart() {
 		if (tickHandler != null) {
--- src/com/bbr/resource/Art.java
+++ src/com/bbr/resource/Art.java
@@ -123,23 +123,16 @@ public final class Art {
 						Utility.printError("Syntax error: Sprite image \"" + curLine + "\" not named in " + SPRITE_LIST + " on line " + lineNumber + " of " + file);
 					} else {
 						float delay = Utility.getFloat(curLine.substring(delimPos+1), -1);
-						System.out.print(curLine.substring(delimPos+1));
 						if (delay <= 0) {
 							delay = Utility.getInt(curLine.substring(delimPos+1), -1);
 						}
 						if (delay <= 0) {
-							System.out.println(": Not valid delay");
 							try {
 								curSprite.addFrame(curLine.substring(0,delimPos), loadImage(curLine.substring(delimPos+1)));
 							} catch (SlickException e) {
 								e.printStackTrace();
 							}
 						} else {
-							System.out.print(": ");
-							System.out.print((delay));
-							System.out.print(" # or frames # ");
-							System.out.println((long)(Settings.valueInt("fps")*delay));
-							System.out.println(curLine.substring(0,delimPos));
 							curSprite.setDelay(curLine.substring(0,delimPos), (long)(Settings.valueInt("fps")*delay));
 						}
 					}
