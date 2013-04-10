b2fba395cb82f2bc25587fbbf696fe393fb01d78
lsftw <lsftw13@gmail.com>
2013-04-10 11:30:58 -0400

    Converted image file reading to use regex.

--- src/com/bbr/resource/ImagesFileReader.java
+++ src/com/bbr/resource/ImagesFileReader.java
@@ -1,12 +1,17 @@
 package com.bbr.resource;
 
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.newdawn.slick.SlickException;
 
 // File reader for images file
 public class ImagesFileReader extends SequentialFileReader {
+	public static final Pattern REGEX_IMAGE_NAME = Pattern.compile("(\\w+)"); // name
+	public static final Pattern REGEX_IMAGE_FILE = Pattern.compile("([^\\.]+\\.[a-zA-Z]+)"); // filename
 	private String curCategory = null;
+
 	public ImagesFileReader(File file) {
 		super(file);
 		cleanLines = true;
@@ -14,7 +19,26 @@ public class ImagesFileReader extends SequentialFileReader {
 	}
 
 	protected void processLine(String curLine, int lineNumber) {
-		if (curLine.indexOf('.') > 0) { // has delim and text before the delim, is file name
+		Matcher imageNameMatcher = REGEX_IMAGE_NAME.matcher(curLine);
+		Matcher imageFileMatcher = REGEX_IMAGE_FILE.matcher(curLine);
+
+		if (imageFileMatcher.matches()) {
+			if (curCategory == null) {
+				Utility.printWarning("Loaded an image without a name on line " + lineNumber +".");
+			} else {
+				if (Art.images.get(curCategory) != null) {
+					Utility.printError("Duplicate image for \"" + curCategory + "\" on line " + lineNumber +".");
+				}
+				try {
+					Art.images.put(curCategory, Art.loadImage(imageFileMatcher.group(1)));
+				} catch (SlickException e) {
+					e.printStackTrace();
+				}
+			}
+		} else if (imageNameMatcher.matches()) {
+			curCategory = imageNameMatcher.group(1);
+		}
+		/*if (curLine.indexOf('.') > 0) { // has delim and text before the delim, is file name
 			if (curCategory == null) {
 				Utility.printWarning("Loaded an image without a name on line " + lineNumber +".");
 			} else {
@@ -29,6 +53,6 @@ public class ImagesFileReader extends SequentialFileReader {
 			}
 		} else {
 			curCategory = curLine;
-		}
+		}*/
 	}
 }
